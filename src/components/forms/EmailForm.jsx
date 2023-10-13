bf97d44cd64e99f8fa171344cb18fd6e9543c2c5
FlakoBB <josephrbb99@gmail.com>
2023-10-13 17:11:25 -0600

    handle message send

--- src/components/forms/EmailForm.jsx
+++ src/components/forms/EmailForm.jsx
@@ -1,34 +1,94 @@
 'use client'
 import styles from '@/styles/emailForm.module.scss'
 import emailjs from '@emailjs/browser'
+import { useRef, useState } from 'react'
 
 const EmailForm = () => {
-  // ToDo: Handle response of send email. Make a toast with the OK or Error
+  const formRef = useRef()
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const messageRef = useRef()
+
+  const [nameError, setNameError] = useState(false)
+  const [emailError, setEmailError] = useState(false)
+  const [messageError, setMessageError] = useState(false)
+  const [toast, setToast] = useState(false)
+  const [errorSend, setErrorSend] = useState(false)
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE, process.env.NEXT_PUBLIC_TEMPLATE, event.target, process.env.NEXT_PUBLIC_PUBLICKEY)
-      .then(response => console.log(response.text))
-      .catch(error => console.log(error.text))
+    let errors = 0
+    if (!nameRef.current.checkValidity()) {
+      setNameError(true)
+      errors += 1
+    } else {
+      setNameError(false)
+    }
+    if (!emailRef.current.checkValidity()) {
+      setEmailError(true)
+      errors += 1
+    } else {
+      setEmailError(false)
+    }
+    if (!messageRef.current.checkValidity()) {
+      setMessageError(true)
+      errors += 1
+    } else {
+      setMessageError(false)
+    }
 
-    event.target.reset()
+    if (errors === 0) {
+      emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE, process.env.NEXT_PUBLIC_TEMPLATE, event.target, process.env.NEXT_PUBLIC_PUBLICKEY)
+        .then(response => {
+          if (response.text === 'OK') {
+            setToast(true)
+            setTimeout(() => {
+              setToast(false)
+            }, 4000)
+            setErrorSend(false)
+          } else {
+            setToast(true)
+            setTimeout(() => {
+              setToast(false)
+            }, 4000)
+            setErrorSend(true)
+          }
+        })
+        .catch(() => {
+          setToast(true)
+          setTimeout(() => {
+            setToast(false)
+          }, 4000)
+          setErrorSend(true)
+        })
+
+      formRef.current.reset()
+    }
   }
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <div className={styles.input_group}>
-        <label htmlFor='name'>Tu Nombre:</label>
-        <input id='name' name='contact_name' />
-        <span className={styles.help_text}>*Este campo es obligatorio</span>
+    <form ref={formRef} className={styles.form} onSubmit={handleSubmit} noValidate>
+      <div className={`${styles.toast} ${toast && styles.show}`}>
+        <p>
+          {
+            errorSend ? 'Error al enviar el mensaje' : 'Mensaje enviado'
+          }
+        </p>
       </div>
       <div className={styles.input_group}>
-        <label htmlFor='email'>Tu Email:</label>
-        <input id='email' name='contact_email' />
-        <span className={styles.help_text}>*Este campo es obligatorio</span>
+        <label htmlFor='name'>Tu Nombre:<span>*</span></label>
+        <input ref={nameRef} type='text' id='name' name='contact_name' required />
+        <span className={`${styles.help_text} ${nameError && styles.active}`}>*Este campo es obligatorio</span>
       </div>
       <div className={styles.input_group}>
-        <label htmlFor='message'>Escribe un mensaje:</label>
-        <textarea id='message' rows='4' name='message' />
-        <span className={styles.help_text}>*Este campo es obligatorio</span>
+        <label htmlFor='email'>Tu Email:<span>*</span></label>
+        <input ref={emailRef} type='email' id='email' name='contact_email' required />
+        <span className={`${styles.help_text} ${emailError && styles.active}`}>*Este campo es obligatorio</span>
+      </div>
+      <div className={styles.input_group}>
+        <label htmlFor='message'>Escribe un mensaje:<span>*</span></label>
+        <textarea ref={messageRef} id='message' rows='4' name='message' required />
+        <span className={`${styles.help_text} ${messageError && styles.active}`}>*Este campo es obligatorio</span>
       </div>
       <button type='submit'>Enviar</button>
     </form>
